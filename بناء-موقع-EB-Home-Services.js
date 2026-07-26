709c462ef3429aedc16620bc11e58cd2f1bd074e
ebmovingcleaning-arch <ebmovingcleaning@gmail.com>
2026-07-26 23:42:10 +0300

    Update بناء-موقع-EB-Home-Services.js

--- بناء-موقع-EB-Home-Services.js
+++ بناء-موقع-EB-Home-Services.js
@@ -1,12 +1,1 @@
-// إرسال إيميل تلقائي
-await fetch(`https://formsubmit.co/ajax/ebmovingcleaning@gmail.com`, {
-  method: 'POST',
-  headers: {'Content-Type': 'application/json'},
-  body: JSON.stringify({
-    name: name,
-    phone: phone,
-    email: email,
-    service: service,
-    message: `حجز جديد من موقع EB Home Services: ${JSON.stringify(formDataObj)}`
-  })
-});
+
